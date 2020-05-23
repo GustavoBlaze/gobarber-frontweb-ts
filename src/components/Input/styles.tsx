854c9ffc85d3e44f3288b811545e976771477b99
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-05-23 19:54:51 -0300

    style: change input autofill style

--- src/components/Input/styles.tsx
+++ src/components/Input/styles.tsx
@@ -24,6 +24,14 @@ export const Container = styled.div`
     &::placeholder {
       color: #666360;
     }
+
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
+      -webkit-box-shadow: 0 0 0 30px #232129 inset !important;
+      -webkit-text-fill-color: #f4ede8 !important;
+    }
   }
 
   svg {
