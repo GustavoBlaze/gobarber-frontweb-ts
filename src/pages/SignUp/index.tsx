fedf8fe17fe722a36ebb78a94ad8175af254e7a1
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-05-23 19:58:10 -0300

    feat: apply unform on signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
 import logo from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -8,6 +10,9 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
   return (
     <Container>
       <Background />
@@ -15,10 +20,10 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logo} alt="gobarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
-          <Input icon={FiUser} name="name" type="text" placeholder="E-mail" />
+          <Input icon={FiUser} name="name" type="text" placeholder="Seu nome" />
           <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
           <Input
             icon={FiLock}
@@ -28,7 +33,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="create">
           <FiArrowLeft />
