8a2fd625098e27db86045f7e766c91eaa52e7bc0
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-05-25 00:38:19 -0300

    chore: redirect user to dashboard on login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FormHandles } from '@unform/core';
 
 import logo from '../../assets/logo.svg';
@@ -25,7 +25,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { signIn } = useAuth();
   const { addToast } = useToast();
-
+  const history = useHistory();
   const handleSubmit = useCallback(
     async (data: FormData) => {
       try {
@@ -41,6 +41,8 @@ const SignIn: React.FC = () => {
         await schema.validate(data, { abortEarly: false });
 
         await signIn({ email: data.email, password: data.password });
+
+        history.push('/dashboard');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -50,12 +52,12 @@ const SignIn: React.FC = () => {
 
         addToast({
           type: 'error',
-          title: 'Houve na autenticação',
+          title: 'Erro na autenticação',
           description: 'Houve um erro na autenticação, verifique seus dados.',
         });
       }
     },
-    [signIn, addToast],
+    [signIn, addToast, history],
   );
 
   return (
